372f36d75583f506dd1997d17bff5352900d34b4
Kiran Mulmi <kiran.mulmi@monotype.com>
2024-03-19 10:17:23 +0545

    User detail integration

--- src/pages/UserManagement/UserDetail.js
+++ src/pages/UserManagement/UserDetail.js
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const UserDetail = () => {
   const { userId } = useParams();
+  const data = [
+    {
+      id: 1,
+      username: 'John',
+      email: 'jon@gmailcom',
+      age: 25,
+      city: 'London'
+    },
+    {
+      id: 2,
+      username: 'Jane',
+      email: 'jane@gmailcom',
+      age: 22,
+      city: 'New York'
+    },
+    {
+      id: 3,
+      username: 'Paul',
+      email: 'paul@gmailcom',
+      age: 30,
+      city: 'Paris'
+    }
+  ];
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    age: "",
+    city: "",
+  });
+
+  useEffect(() => {
+    const newUser = data.find((obj) => obj.id.toString() === userId.toString())
+    if(newUser) {
+      setUser(newUser);
+    }
+  }, []);
+
   return (
     <div>
-      User detail page {userId}
+      <h2>User Detail</h2>
+      <div>Username: {user.username}</div>
+      <div>Email: {user.email}</div>
+      <div>Age: {user.age}</div>
+      <div>City: {user.city}</div>
     </div>
   )
 }
